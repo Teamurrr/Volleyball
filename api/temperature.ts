19a17fd8157169877d00ad1d87e73d0dff921314
ggtimurr <timur23@kstu.kg>
2026-05-20 14:46:54 +0600

    api route

--- api/temperature.ts
+++ api/temperature.ts
@@ -1,5 +1,23 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+import { initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwtjj3eIS-rFVfzZhF4QmWEHImwDpkDvA",
+  authDomain: "volleyball-c0949.firebaseapp.com",
+  projectId: "volleyball-c0949",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
 export default async function handler(
   req: VercelRequest,
   res: VercelResponse
@@ -11,7 +29,12 @@ export default async function handler(
     });
   }
 
-  console.log(req.body);
+  const { temperature } = req.body;
+
+  await setDoc(doc(db, "sensors", "room1"), {
+    temperature,
+    updatedAt: Date.now(),
+  });
 
   return res.status(200).json({
     success: true,
