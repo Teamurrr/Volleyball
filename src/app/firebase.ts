d663f5650e7113ef994bcd88b514d8cc61f4810e
ggtimurr <timur23@kstu.kg>
2026-05-26 23:42:20 +0600

    firebase config fix 2

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
@@ -12,7 +12,20 @@ const firebaseConfig = {
   measurementId: "G-WVG8SBY4GD"
 };
 
+const temperatureFirebaseConfig = {
+  apiKey: "AIzaSyC929fAMDt9rPegQWLb_veehh67Bn5dZ0s",
+  authDomain: "temperaturedata-68177.firebaseapp.com",
+  databaseURL: "https://temperaturedata-68177-default-rtdb.firebaseio.com",
+  projectId: "temperaturedata-68177",
+  storageBucket: "temperaturedata-68177.firebasestorage.app",
+  messagingSenderId: "819491718849",
+  appId: "1:819491718849:web:3f5b0cb01a3d30947fb640",
+  measurementId: "G-ZVT9WSNQV0"
+};
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const temperatureApp =
+  getApps().find((currentApp) => currentApp.name === "temperature") ??
+  initializeApp(temperatureFirebaseConfig, "temperature");
 export const db = getFirestore(app);
-export const database = getDatabase(app);
+export const database = getDatabase(temperatureApp);
